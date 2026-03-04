4314a397ab2f2e1ffdcfa4200a85c189b310989d
Matheus Henrique <matheus002_@hotmail.com>
2026-03-04 09:28:34 -0300

    add concurrency validation test in the manager

--- manager/main_test.go
+++ manager/main_test.go
@@ -24,3 +24,34 @@ func TestGetTruck(t *testing.T) {
 		t.Errorf("Expected truck ID to be 1, got %s", truck.ID)
 	}
 }
+
+func TestConcurrentUpdate(t *testing.T) {
+	manager := NewTruckManager()
+	manager.AddTruck("1", 100)
+
+	const numGoroutines = 100
+	const iterations = 100
+
+	done := make(chan bool)
+
+	for i := 0; i < iterations; i++ {
+		go func() {
+			for j := 0; j < iterations; j++ {
+				truck, _ := manager.GetTruck("1")
+				manager.UpdateTruckCargo("1", truck.Cargo+1)
+			}
+			done <- true
+		}()
+	}
+
+	for range numGoroutines {
+		<-done
+	}
+
+	expectedFinalValue := numGoroutines*iterations + 100
+	finalTruck, _ := manager.GetTruck("1")
+
+	if finalTruck.Cargo != expectedFinalValue {
+		t.Errorf("Expected final cargo to be %d, but got %d. Race condition detected!", expectedFinalValue, finalTruck.Cargo)
+	}
+}
